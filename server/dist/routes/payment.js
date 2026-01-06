4d4d2eff3fc7f3d5aa3a64d6f91393dfb52eccff
kankan ray <kankanray059@gmail.com>
2026-01-07 01:51:54 +0530

    render 4

--- server/dist/routes/payment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const paymentControllers_1 = require("../controllers/paymentControllers");
-const router = (0, express_1.Router)();
-router.post("/create-order", paymentControllers_1.createOrder);
-router.post("/verify", paymentControllers_1.verifyPayment);
-exports.default = router;
