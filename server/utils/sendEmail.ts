a0e8d3f6921018a68f17b60fc89c13a8e92b7fc6
kankan ray <kankanray059@gmail.com>
2026-01-31 00:07:36 +0530

    fix sending mail

--- server/utils/sendEmail.ts
+++ server/utils/sendEmail.ts
@@ -1,15 +1,20 @@
-// import * as nodemailer from "nodemailer";
 import nodemailer from "nodemailer";
+
 export async function sendEmail(
   to: string,
   subject: string,
   html: string
 ) {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, 
     auth: {
-      user: process.env.EMAIL_USER,
+      user: process.env.EMAIL_USER, 
       pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   });
 
